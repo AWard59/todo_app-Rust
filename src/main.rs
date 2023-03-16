84437ee8ef1ad53ff7595f3ffbe0fe607d394284
AWard59 <iamsancho5990@hotmail.com>
2023-03-16 16:50:00 -0700

    remove old code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
+struct TodoItem {
+    id: u32,
+    name: String,
+    completed: bool,
+}
+
 fn main() {
     let mut todo_list: Vec<TodoItem> = Vec::new();
 
@@ -58,23 +64,6 @@ fn main() {
         }
 
     }
-
-    // let item1 = TodoItem {
-    //     id: 1,
-    //     name: String::from("Buy milk"),
-    //     completed: false,
-    // };
-
-    // let item2 = TodoItem {
-    //     id: 2,
-    //     name: String::from("Walk the dog"),
-    //     completed: false,
-    // };
-
-    // todo_list.push(item1);
-    // todo_list.push(item2);
-
-    // println!("{:?}", todo_list);
 }
 
 fn complete_item(item: &mut TodoItem) {
@@ -85,10 +74,4 @@ fn display_items(items: &Vec<TodoItem>) {
     for item in items {
         println!("{} - {} ({})", item.id, item.name, item.completed);
     }
-}
-
-struct TodoItem {
-    id: u32,
-    name: String,
-    completed: bool,
 }
